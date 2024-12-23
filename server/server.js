5650dbc2cb9466996d763a30921ea4e443da2661
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2024-12-23 15:54:27 -0300

    criando a base do express

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const porta = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.get('/contato', (req, res) => {
+    res.send('Pagina de contato')
+})
+app.listen(porta, () => {
+    console.log(`Servidor iniciado na porta ${porta}`)
+});
